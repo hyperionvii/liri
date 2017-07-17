fd33e4cc5780533c4b395ba86ed39c33b9d9ee53
Sofia Wicker <sofiazw@gmail.com>
2017-07-17 17:28:25 -0400

    update first files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'W38VLG61Bh8YUri2U345SkOSA',
+  consumer_secret: '8DXVFtvWg1slC55oLfgwiruvynzHD5Rkfh41Nu6SkWiUfocTeP',
+  access_token_key: '887012885645414400-Zv7d2HIuqEDdv94hOv64HQJjONDv0N4',
+  access_token_secret: 'jJuc0FQgnxXCEW5kGepsr74y8yoIiOheNAS48RIg31PM4',
+}
